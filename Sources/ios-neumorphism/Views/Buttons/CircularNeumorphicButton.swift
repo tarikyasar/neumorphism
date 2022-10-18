aa191ef0c871307c986a4696d607966fbdcbaff2
Tarik Yasar <m.tarikyasar@gmail.com>
2022-10-18 14:28:08 +0300

    Init view for test

--- Sources/ios-neumorphism/Views/Buttons/CircularNeumorphicButton.swift
+++ Sources/ios-neumorphism/Views/Buttons/CircularNeumorphicButton.swift
@@ -8,9 +8,15 @@
 import SwiftUI
 
 public struct CircularNeumorphicButton<Label>: View where Label: View {
-    var action: () -> Void
-    var label: Label
-    var isDarkModeEnabled: Bool
+    public var action: () -> Void
+    public var label: Label
+    public var isDarkModeEnabled: Bool
+    
+    public init(_ action: @escaping () -> Void, _ label: Label, _ isDarkModeEnabled: Bool) {
+        self.action = action
+        self.label = label
+        self.isDarkModeEnabled = isDarkModeEnabled
+    }
     
     public var body: some View {
         if (isDarkModeEnabled) {
