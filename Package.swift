d912394cdaaef34fb3d97199659a5d1abde02d63
Tarik Yasar <m.tarikyasar@gmail.com>
2023-05-04 19:06:15 +0300

    Update package name

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "ios-neumorphism",
+    name: "Neumorphism",
     platforms: [
         .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ios-neumorphism",
-            targets: ["ios-neumorphism"]),
+            name: "Neumorphism",
+            targets: ["Neumorphism"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "ios-neumorphism",
+            name: "Neumorphism",
             dependencies: []),
         .testTarget(
-            name: "ios-neumorphismTests",
-            dependencies: ["ios-neumorphism"]),
+            name: "NeumorphismTests",
+            dependencies: ["Neumorphism"]),
     ]
 )
